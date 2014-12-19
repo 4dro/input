2edf9445589ba823eae5c2b7be5766f5cf2a76fe
droopy <droopy@yandex.ru>
2014-12-19 22:46:51 +0600

    Creating inputs

--- web/droopy/input/NumberInput.js
+++ web/droopy/input/NumberInput.js
@@ -1,21 +1,52 @@
 define([
 	'dojo/_base/declare',
-	'dijit/_WidgetBase'
-], function(declare, _WidgetBase){
+	'dijit/_WidgetBase',
+	'dojo/dom-class',
+	'dojo/on',
+	'dojo/keys'
+], function(declare, _WidgetBase, domClass, on, keys){
 return declare([_WidgetBase], {
+	// public properties
 	inputs: null,
 	separators: null,
+	fraction: 2,
 
-	constructor: function(args)
+	// internal properties
+	_inputFields: null,
+	_sepFields: null,
+
+	constructor: function()
 	{
-		this.inputs = [];
-		this.separators = [];
-		declare.safeMixin(this, args);
+		this.inputs = [999, 999, 99];
+		this.separators = [' ', '.'];
 	},
 
 	buildRendering: function()
 	{
 		this.inherited(arguments);
+		domClass.add(this.domNode, 'droopy-input');
+		this._inputFields = [];
+		for (var i = 0; i < this.inputs.length; i++)
+		{
+			var inp = document.createElement('input');
+			var val = this.inputs[i];
+			inp.setAttribute('type', 'text');
+			inp.setAttribute('maxlength', val.toString().length);
+			inp.setAttribute('data-idx', i);
+
+			this._inputFields.push(inp);
+			this.domNode.appendChild(inp);
+			this.own(on(inp, 'keypress', function(e){
+
+			}));
+		}
+		this._sepFields = [];
+		for (i = 0; i < this.separators.length; i++)
+		{
+			var sep = document.createElement('span');
+			this._sepFields.push(sep);
+			this.domNode.appendChild(sep);
+		}
 	}
 });
 });
--- web/droopy/input/TimeInput.js
+++ web/droopy/input/TimeInput.js
@@ -4,5 +4,20 @@ define([
 ], function(declare, NumberInput){
 return declare([NumberInput], {
 
+    useHours: true,
+    useMinutes: true,
+    useSeconds: true,
+
+
+    constructor: function()
+    {
+        this.inputs = [23, 59, 59];
+        this.separators = [':', ':'];
+    },
+
+    buildRendering: function()
+    {
+        this.inherited(arguments);
+    }
 });
 });
